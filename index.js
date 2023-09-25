23def1e68d44c4662cc8b06deaa3a2619ea17bd6
121110669 GABRIEL DANTAS DE OLIVEIRA <gabriel.dantas.oliveira@lcc2-53.lcc.ufcg.edu.br>
2023-09-25 10:25:29 -0300

    Starting

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Arquivo central do backend
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongo DB!!!");
+    } catch (error) {
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected", () =>{
+    console.log("Mongo DB disconnected!!!");
+});
+
+
+// Middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+// Rotas Aqui
+// app.use("/auth", authRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+app.listen(8800, () => {
+    connect();
+    console.log("Connected to backend.");
+});
